6f67e4d971be763f5874720bc911993aa31aa4d5
Polushin Dima <dima2004polushin2004@gmail.com>
2023-07-20 23:29:51 +0600

    Создал компонент CardBlog.vue и сделал верстку

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 
+import Header from "@/components/Header.vue";
+import CardBlog from "@/components/CardBlog.vue";
 </script>
 
 <template>
-  <header>
-    Привет мир !
-  </header>
+  <Header/>
+  <main>
+
+    <div class="container text-center">
+      <div class="row row-cols-3">
+        <div class="col">
+          <CardBlog/>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/CardBlog.vue
@@ -0,0 +1,23 @@
+
+<script lang="ts">
+
+</script>
+
+<template>
+  <div class="card">
+    <img src="../assets/image/blogImage.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="d-flex justify-content-between align-items-center bg-dark-subtle p-1 mb-3">
+          <p class="card-text m-0">Дата: </p>
+          <p class="card-text m-0">Создатель: </p>
+        </div>
+        <a href="#" class="btn btn-primary">Подробнее...</a>
+      </div>
+    </div>
+</template>
+
+<style>
+
+</style>
