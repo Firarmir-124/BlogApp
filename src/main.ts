a54a82685057d61aea734521ff6739ff6e9ef667
Polushin Dima <dima2004polushin2004@gmail.com>
2023-07-20 23:14:51 +0600

    Создал компонент Layout.vue и сделал верстку

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 
 const app = createApp(App)
@@ -8,3 +8,5 @@ const app = createApp(App)
 app.use(createPinia())
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap'
