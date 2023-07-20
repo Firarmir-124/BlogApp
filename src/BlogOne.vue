49e92721599c46a7fd39ab52a298da31617738bb
Polushin Dima <dima2004polushin2004@gmail.com>
2023-07-20 23:39:42 +0600

    Создал компонент BlogOne.vue и сделал верстку

---
+++ src/BlogOne.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <a class="btn btn-secondary my-3" role="button">Назад</a>
+  <div class="card" style="width: 100%">
+    <img src="./assets/image/blogImage.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h3 class="card-title">Card title</h3>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Дата создания</li>
+          <li class="list-group-item">Создатель</li>
+        </ul>
+      </div>
+      <div class="btn-group mt-3" role="group" aria-label="Basic outlined example">
+        <button type="button" class="btn btn-outline-primary">Удалить</button>
+        <button type="button" class="btn btn-outline-primary">Редактировать</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
