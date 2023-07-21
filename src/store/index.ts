bd3527942b5cf36566d9230ffa1e8d09246ede6f
Polushin Dima <dima2004polushin2004@gmail.com>
2023-07-21 12:46:30 +0600

    Подключил persister и сохранил посты в localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
 import { createStore } from 'vuex'
 import { blogModule } from '@/store/blogModule'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
   modules: {
     blog: blogModule
-  }
+  },
+  plugins: [
+    createPersistedState({
+      key: 'blog',
+      patch: 'blog.blogList',
+      storage: window.localStorage
+    })
+  ]
 })
